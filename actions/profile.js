ec7ed62dfb2f64bf772d2e13def5e2b5647589c2
amir_ramezani <ramezaniamir47@gmail.com>
2025-04-14 20:44:53 +0330

    create dleteform component and write deleteaddress action and  use revalidatepath

--- actions/profile.js
+++ actions/profile.js
@@ -1,6 +1,7 @@
 "use server";
 import { postFetch } from "@/utils/fetch";
 import { handleError } from "@/utils/helper";
+import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
 async function editInfo(state, formData) {
@@ -95,6 +96,7 @@ async function createAddress(state, formData) {
     { Authorization: `Bearer ${token.value}` }
   );
   if (data.status === "success") {
+    revalidatePath("/profile/addresses");
     return {
       status: data.status,
       message: "ثبت آدرس با موفقیت انجام شد",
@@ -183,5 +185,35 @@ async function editAddress(state, formData) {
     };
   }
 }
+async function deleteAddress(state, formData) {
+  const address_id = formData.get("address_id");
 
-export { editInfo, createAddress, editAddress };
+  if (address_id === null || address_id === "") {
+    return {
+      status: "error",
+      message: "شناسه آدرس الزامی است.",
+    };
+  }
+
+  const token = cookies().get("token");
+  const data = await postFetch(
+    "/profile/addresses/delete",
+    {
+      address_id,
+    },
+    { Authorization: `Bearer ${token.value}` }
+  );
+  if (data.status === "success") {
+    revalidatePath("/profile/addresses");
+    return {
+      status: data.status,
+      message: "حذف آدرس با موفقیت انجام شد",
+    };
+  } else {
+    return {
+      status: data.status,
+      message: handleError(data.message),
+    };
+  }
+}
+export { editInfo, createAddress, editAddress, deleteAddress };
--- components/profile/addresses/EditForm.js
+++ components/profile/addresses/EditForm.js
@@ -1,20 +1,23 @@
 "use client";
 
-import { editAddress } from "@/actions/profile";
+import { deleteAddress, editAddress } from "@/actions/profile";
 import SubmitButton from "@/components/SubmitButton";
 import { useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 import { toast } from "react-toastify";
+import DeleteForm from "./DeleteForm";
 
 export default function EditForm({ address, cities, provinces }) {
   const [citiesFilter, setCitiesFilter] = useState(
     cities.filter((city) => city.province_id === provinces[0].id)
   );
   const [stateEdit, formActionEdit] = useFormState(editAddress, {});
+  const [stateDelete, formActionDelete] = useFormState(deleteAddress, {});
 
   useEffect(() => {
     toast(stateEdit?.message, { type: `${stateEdit?.status}` });
-  }, [stateEdit]);
+    toast(stateDelete?.message, { type: `${stateDelete?.status}` });
+  }, [stateEdit, stateDelete]);
 
   function changeProvince(e) {
     setCitiesFilter(
@@ -23,82 +26,82 @@ export default function EditForm({ address, cities, provinces }) {
   }
   return (
     <>
-      <form action={formActionEdit} className="card card-body mt-3">
-        <div className="row g-4">
-          <div className="col col-md-6">
-            <label className="form-label">عنوان</label>
-            <input
-              name="title"
-              type="text"
-              defaultValue={address.title}
-              className="form-control"
-            />
+      <div className="position-relative">
+        <form action={formActionEdit} className="card card-body mt-3">
+          <div className="row g-4">
+            <div className="col col-md-6">
+              <label className="form-label">عنوان</label>
+              <input
+                name="title"
+                type="text"
+                defaultValue={address.title}
+                className="form-control"
+              />
+            </div>
+            <div className="col col-md-6">
+              <label className="form-label">شماره تماس</label>
+              <input
+                name="cellphone"
+                type="text"
+                defaultValue={address.cellphone}
+                className="form-control"
+              />
+            </div>
+            <div className="col col-md-6">
+              <label className="form-label">کد پستی</label>
+              <input
+                name="postal_code"
+                type="text"
+                defaultValue={address.postal_code}
+                className="form-control"
+              />
+            </div>
+            <div className="col col-md-6">
+              <label className="form-label">استان</label>
+              <select
+                name="province_id"
+                defaultValue={address.province_id}
+                className="form-select"
+                onChange={changeProvince}
+              >
+                {provinces.map((province) => (
+                  <option key={province.id} value={province.id}>
+                    {province.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col col-md-6">
+              <label className="form-label">شهر</label>
+              <select
+                name="city_id"
+                defaultValue={address.city_id}
+                className="form-select"
+              >
+                {citiesFilter.map((city) => (
+                  <option key={city.id} value={city.id}>
+                    {city.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col col-md-12">
+              <label className="form-label">آدرس</label>
+              <textarea
+                name="address"
+                defaultValue={address.address}
+                rows="5"
+                className="form-control"
+              ></textarea>
+            </div>
+            <input type="hidden" name="address_id" value={address.id} />
           </div>
-          <div className="col col-md-6">
-            <label className="form-label">شماره تماس</label>
-            <input
-              name="cellphone"
-              type="text"
-              defaultValue={address.cellphone}
-              className="form-control"
-            />
+          <div>
+            <SubmitButton title="ویرایش" style="btn btn-primary mt-4" />
           </div>
-          <div className="col col-md-6">
-            <label className="form-label">کد پستی</label>
-            <input
-              name="postal_code"
-              type="text"
-              defaultValue={address.postal_code}
-              className="form-control"
-            />
-          </div>
-          <div className="col col-md-6">
-            <label className="form-label">استان</label>
-            <select
-              name="province_id"
-              defaultValue={address.province_id}
-              className="form-select"
-              onChange={changeProvince}
-            >
-              {provinces.map((province) => (
-                <option key={province.id} value={province.id}>
-                  {province.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="col col-md-6">
-            <label className="form-label">شهر</label>
-            <select
-              name="city_id"
-              defaultValue={address.city_id}
-              className="form-select"
-            >
-              {citiesFilter.map((city) => (
-                <option key={city.id} value={city.id}>
-                  {city.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="col col-md-12">
-            <label className="form-label">آدرس</label>
-            <textarea
-              name="address"
-              defaultValue={address.address}
-              rows="5"
-              className="form-control"
-            ></textarea>
-          </div>
-          <input type="hidden" name="address_id" value={address.id} />
-        </div>
-        <div className="d-flex justify-content-between mt-4">
-          <SubmitButton title="ویرایش" style="btn btn-primary mt-4" />
-          <button type="submit" className="btn btn-dark mt-4">
-            حذف
-          </button>
-        </div>
-      </form>
+        </form>
+        <DeleteForm addressId={address.id} />
+      </div>
     </>
   );
 }
