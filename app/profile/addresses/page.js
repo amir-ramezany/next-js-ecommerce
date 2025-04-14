6614b96f05852796343d7bc4eb7bc9c646ca9da6
amir_ramezani <ramezaniamir47@gmail.com>
2025-04-14 12:00:21 +0330

    create editform component and fix its bug

--- app/profile/addresses/page.js
+++ app/profile/addresses/page.js
@@ -1,4 +1,5 @@
 import CreateForm from "@/components/profile/addresses/CreateForm";
+import EditForm from "@/components/profile/addresses/EditForm";
 import { getFetch } from "@/utils/fetch";
 import { cookies } from "next/headers";
 
@@ -15,51 +16,15 @@ export default async function AddressesPage() {
     <>
       <CreateForm provinces={provinces} cities={cities} />
       <hr />
-      {/* <div class="card card-body">
-        <div class="row g-4">
-          <div class="col col-md-6">
-            <label class="form-label">عنوان</label>
-            <input type="text" value="منزل" class="form-control" />
-          </div>
-          <div class="col col-md-6">
-            <label class="form-label">شماره تماس</label>
-            <input type="text" value="09111111111" class="form-control" />
-          </div>
-          <div class="col col-md-6">
-            <label class="form-label">کد پستی</label>
-            <input type="text" value="4586-2115" class="form-control" />
-          </div>
-          <div class="col col-md-6">
-            <label class="form-label">استان</label>
-            <select class="form-select">
-              <option selected>تهران</option>
-              <option value="1">اصفهان</option>
-              <option value="2">فارس</option>
-              <option value="3">یزد</option>
-            </select>
-          </div>
-          <div class="col col-md-6">
-            <label class="form-label">شهر</label>
-            <select class="form-select">
-              <option selected>تهران</option>
-              <option value="1">اصفهان</option>
-              <option value="2">شیراز</option>
-              <option value="3">یزد</option>
-            </select>
-          </div>
-          <div class="col col-md-12">
-            <label class="form-label">آدرس</label>
-            <textarea type="text" rows="5" class="form-control">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است.
-            </textarea>
-          </div>
-        </div>
-        <div class="d-flex justify-content-between mt-4">
-          <button class="btn btn-primary">ویرایش</button>
-          <button class="btn btn-dark">حذف</button>
-        </div>
-      </div> */}
+      {addresses.map((address) => (
+        <EditForm
+          key={address.id}
+          address={address}
+          provinces={provinces}
+          cities={cities}
+        />
+      ))}
+
       {/* <hr />
         <div class="card card-body">
           <div class="row g-4">
---
+++ components/profile/addresses/EditForm.js
@@ -0,0 +1,102 @@
+"use client";
+
+import { editAddress } from "@/actions/profile";
+import SubmitButton from "@/components/SubmitButton";
+import { useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+
+export default function EditForm({ address, cities, provinces }) {
+  const [citiesFilter, setCitiesFilter] = useState(
+    cities.filter((city) => city.province_id === provinces[0].id)
+  );
+  const [stateEdit, formActionEdit] = useFormState(editAddress, {});
+
+  useEffect(() => {
+    toast(stateEdit?.message, { type: `${stateEdit?.status}` });
+  }, [stateEdit]);
+
+  function changeProvince(e) {
+    setCitiesFilter(
+      cities.filter((city) => city.province_id === +e.target.value)
+    ); //province_id is number
+  }
+  return (
+    <>
+      <form action={formActionEdit} className="card card-body mt-3">
+        <div className="row g-4">
+          <div className="col col-md-6">
+            <label className="form-label">عنوان</label>
+            <input
+              name="title"
+              type="text"
+              defaultValue={address.title}
+              className="form-control"
+            />
+          </div>
+          <div className="col col-md-6">
+            <label className="form-label">شماره تماس</label>
+            <input
+              name="cellphone"
+              type="text"
+              defaultValue={address.cellphone}
+              className="form-control"
+            />
+          </div>
+          <div className="col col-md-6">
+            <label className="form-label">کد پستی</label>
+            <input
+              name="postal_code"
+              type="text"
+              defaultValue={address.postal_code}
+              className="form-control"
+            />
+          </div>
+          <div className="col col-md-6">
+            <label className="form-label">استان</label>
+            <select
+              name="province_id"
+              defaultValue={address.province_id}
+              className="form-select"
+              onChange={changeProvince}
+            >
+              {provinces.map((province) => (
+                <option key={province.id} value={province.id}>
+                  {province.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="col col-md-6">
+            <label className="form-label">شهر</label>
+            <select
+              name="city_id"
+              defaultValue={address.city_id}
+              className="form-select"
+            >
+              {citiesFilter.map((city) => (
+                <option key={city.id} value={city.id}>
+                  {city.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="col col-md-12">
+            <label className="form-label">آدرس</label>
+            <textarea
+              name="address"
+              defaultValue={address.address}
+              rows="5"
+              className="form-control"
+            ></textarea>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between mt-4">
+          <SubmitButton title="ویرایش" style="btn btn-primary mt-4" />
+          <button type="submit" className="btn btn-dark">
+            حذف
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
