9fdffc39d782efd514874682926192f63baa69d1
amir_ramezani <ramezaniamir47@gmail.com>
2025-04-19 21:22:48 +0330

    create transactions page with its components

--- app/profile/orders/page.js
+++ app/profile/orders/page.js
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 
 export default function OrdersPage({ searchParams }) {
   const params = new URLSearchParams(searchParams);
+  // --params.toString() -> page=1
   return (
     <Suspense key={params.toString()} fallback={<Loading />}>
       <Table params={params.toString()} />
