1299e1fc55a38fba63f933533bee3e0f39a793c7
amir_ramezani <ramezaniamir47@gmail.com>
2025-04-21 02:52:19 +0330

    using redux and create cartslice for shopping cart

--- app/layout.js
+++ app/layout.js
@@ -5,19 +5,22 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import NextProgress from "@/components/libraries/NextProgress";
 import AuthConextProvider from "@/context/AuthContext";
+import Providers from "@/redux/Provider";
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
         <AuthConextProvider>
-          <NextProgress>
-            <Header />
-            {children}
+          <Providers>
+            <NextProgress>
+              <Header />
+              {children}
 
-            <Footer />
-            <BootstrapClient />
-            <Toastify />
-          </NextProgress>
+              <Footer />
+              <BootstrapClient />
+              <Toastify />
+            </NextProgress>
+          </Providers>
         </AuthConextProvider>
       </body>
     </html>
